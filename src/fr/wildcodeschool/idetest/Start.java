05af6a9933842c8c15cbcb9100aecbef38fa636f
kobanogit <mlme@disroot.org>
2018-04-05 13:56:52 +0200

    modif chemin fichier start

---
+++ src/fr/wildcodeschool/idetest/Start.java
@@ -0,0 +1,17 @@
+package fr.wildcodeschool.idetest;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+
+public class Start {
+    public static void main(String... args) throws IOException {
+        System.out.println(ProcessHandle.current().pid());
+
+        JFrame jf = new JFrame("Gotcha!");
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.add(new JLabel(new ImageIcon(new URL("http://tumalo.redmond.k12.or.us/files/2013/08/apple-touch-icon-precomposed.png"))));
+        jf.pack();
+        jf.setVisible(true);
+    }
+}
